90baa38e07ee54cf2966deae1475858ad36db8eb
Jesse Sher <jebba272727@gmail.com>
2026-03-16 12:20:12 +1100

    made that it gets the suburb you are in so its more accurate

--- src/main.cpp
+++ src/main.cpp
@@ -21,10 +21,10 @@ const int WIDTH = 850;
 const int HEIGHT = 450;
 Color BG_COLOR = Color {0, 149, 194, 255};
 
-std::string city_global = "Loading...";
-void set_city(std::string city)
+std::string area_global = "Loading...";
+void set_area(std::string city)
 {
-	city_global = city;
+	area_global = city;
 }
 
 std::string weather_desc = "Loading...";
@@ -41,7 +41,7 @@ void set_temperature(std::string temperature_inp)
 
 EMSCRIPTEN_BINDINGS(my_module)
 {
-	emscripten::function("set_city", &set_city);
+	emscripten::function("set_area", &set_area);
 	emscripten::function("set_weather_desc", &set_weather_desc);
 	emscripten::function("set_temperature", &set_temperature);
 }
@@ -76,7 +76,7 @@ void UpdateDrawFrame()
 	
 	ClearBackground(BG_COLOR);
 
-	std::string city_str = "City: " + city_global;
+	std::string city_str = "Area: " + area_global;
 	DrawText(city_str.c_str(), 20, 20, 20, BLACK);
 
 	std::string weather_desc_str = "Weather: " + weather_desc;
